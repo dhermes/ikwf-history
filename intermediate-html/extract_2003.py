b59a3cd1a1ba98a63ea1bbb1eabba5af41868e4c
Danny Hermes <daniel.j.hermes@gmail.com>
2025-03-26 15:58:26 -0500

    Partially update 2003.

--- intermediate-html/extract_2003.py
+++ intermediate-html/extract_2003.py
@@ -880,7 +880,14 @@ def main():
 
     with open(HERE / "extracted.2003.json", "w") as file_obj:
         json.dump(
-            [weight_class.model_dump() for weight_class in parsed], file_obj, indent=2
+            {
+                "weight_classes": [
+                    weight_class.model_dump() for weight_class in parsed
+                ],
+                "team_scores": {},
+            },
+            file_obj,
+            indent=2,
         )
         file_obj.write("\n")
 
