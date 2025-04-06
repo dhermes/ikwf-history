c5b9cc821cd3e9809e9d22af22a21fd71ca200b2
Danny Hermes <daniel.j.hermes@gmail.com>
2025-04-05 20:33:01 -0500

    Extracted 2016.

---
+++ intermediate-html/extract_2016.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2025 - Present. IKWF History. All rights reserved.
+
+import pathlib
+from typing import Any
+
+import bracket_utils
+import trackwrestling
+
+HERE = pathlib.Path(__file__).resolve().parent
+_NAME_FIXES: dict[str, str] = {
+    "Christophe Carr": "Christopher Carr",
+    "Christophe Moore": "Christopher Moore",
+    "Christophe Worcester": "Christopher Worcester",
+    "David-Jose Rice": "David-Joseph Rice",
+    "Noah Manue Tapia": "Noah Manuel Tapia",
+    "Tuvshinjar Zuunbayan": "Tuvshinjargal Zuunbayan",
+}
+_NAME_EXCEPTIONS: dict[tuple[str, str], bracket_utils.Competitor] = {
+    ("J Guido Pigoni", "SCN Youth WC"): bracket_utils.Competitor(
+        first_name="J Guido", last_name="Pigoni", suffix=None, team="SCN Youth WC"
+    ),
+    ("Jujuan Williams Jr", "Lionheart Intense Wrestling"): bracket_utils.Competitor(
+        first_name="Jujuan",
+        last_name="Williams",
+        suffix="Jr",
+        team="Lionheart Intense Wrestling",
+    ),
+    ("Mustafa St. Clair", "Harvey Twisters WC"): bracket_utils.Competitor(
+        first_name="Mustafa",
+        last_name="St. Clair",
+        suffix=None,
+        team="Harvey Twisters WC",
+    ),
+    ("Noah Manuel Tapia", "Young Guns Wrestling"): bracket_utils.Competitor(
+        first_name="Noah Manuel",
+        last_name="Tapia",
+        suffix=None,
+        team="Young Guns Wrestling",
+    ),
+}
+
+
+def _parse_rounds(
+    selenium_rounds: Any,
+    match_slots_by_bracket: trackwrestling.MatchSlotsByBracket,
+) -> list[trackwrestling.MatchWithBracket]:
+    if not isinstance(selenium_rounds, dict):
+        raise TypeError("Unexpected value", type(selenium_rounds))
+
+    matches: list[trackwrestling.MatchWithBracket] = []
+
+    matches.extend(
+        trackwrestling.parse_r32(
+            "Championship Preliminary",
+            "Champ. Round 1",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_r16(
+            "Championship 1st Round",
+            "Champ. Round 2",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_quarterfinal(
+            "Championship Quarterfinals",
+            "Quarterfinal",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_consolation_round3(
+            "Consolation Preliminary",
+            "Cons. Round 1",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_consolation_round4(
+            "Consolation 1st Round",
+            "Cons. Round 2",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_semi_mixed(
+            "Champ Semi`s & Con Quarterfinal",
+            "Semifinal",
+            "Cons. Round 3",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_consolation_semi(
+            "Consolation Semifinal",
+            "Cons. Semi",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_place_matches_v2(
+            "Place Bouts",
+            "3rd Place Match",
+            "5th Place Match",
+            "7th Place Match",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    matches.extend(
+        trackwrestling.parse_championship_matches(
+            "Championship Bouts",
+            "1st Place Match",
+            selenium_rounds,
+            match_slots_by_bracket,
+            _NAME_EXCEPTIONS,
+        )
+    )
+
+    if selenium_rounds:
+        raise ValueError("Round not processed", selenium_rounds.keys())
+
+    return matches
+
+
+def main():
+    root = HERE.parent / "raw-data" / "2016"
+    extracted_tournament = trackwrestling.extract_year(root, _parse_rounds, _NAME_FIXES)
+    with open(HERE / "extracted.2016.json", "w") as file_obj:
+        file_obj.write(extracted_tournament.model_dump_json(indent=2))
+        file_obj.write("\n")
+
+
+if __name__ == "__main__":
+    main()
