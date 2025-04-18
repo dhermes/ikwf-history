8d47c2e42b407be56bcd330d222dd4a6812e1f71
Danny Hermes <daniel.j.hermes@gmail.com>
2025-04-17 21:07:36 -0500

    Add champs and placers.

--- database-v2/write_html.py
+++ database-v2/write_html.py
@@ -124,43 +124,109 @@ def _get_weight_ref_html(
     return f'<a href="{url}">{html.escape(weight_text)}</a>'
 
 
-def _get_placement_suffix(place: int | None) -> str:
+def _get_placement_suffix(place: int | None, year: int | None = None) -> str:
     if place is None:
+        if year is not None:
+            raise NotImplementedError("Cannot render year without placement")
+
         return ""
 
+    year_text = ""
+    if year is not None:
+        year_text = f"{year}, "
+
     if place == 1:
-        return " (Champion)"
+        return f" ({year_text}Champion)"
+
     if place == 2:
-        return " (2nd place)"
+        return f" ({year_text}2nd place)"
+
     if place == 3:
-        return " (3rd place)"
+        return f" ({year_text}3rd place)"
+
     if place == 4:
-        return " (4th place)"
+        return f" ({year_text}4th place)"
+
     if place == 5:
-        return " (5th place)"
+        return f" ({year_text}5th place)"
+
     if place == 6:
-        return " (6th place)"
+        return f" ({year_text}6th place)"
+
     if place == 7:
-        return " (7th place)"
+        return f" ({year_text}7th place)"
+
     if place == 8:
-        return " (8th place)"
+        return f" ({year_text}8th place)"
 
     raise NotImplementedError(place)
 
 
-def _get_team_html(
-    static_root: pathlib.Path, team: TeamInfo, qualifiers: list[Qualifier]
-) -> str:
-    name = team.name_normalized
+def _get_champs_html_parts(
+    static_root: pathlib.Path, qualifiers: list[Qualifier]
+) -> list[str]:
+    champions: list[Qualifier] = []
+    for qualifier in qualifiers:
+        if qualifier.place == 1:
+            champions.append(qualifier)
+
+    if len(champions) == 0:
+        return []
+
     parts: list[str] = [
-        "<html>",
-        "  <head>",
-        f"    <title>Team: {html.escape(name)}</title>",
-        '    <link href="/css/tournament-view.fb678ab0.min.css" rel="stylesheet" />',
-        "  </head>",
-        "  <body>",
-        '    <div class="tournament-view">',
-        f"      <h1>{html.escape(name)}</h1>",
+        f"<h2>State Champions ({len(champions)})</h2>",
+        "<ol>",
+    ]
+
+    for qualifier in champions:
+        weight_link = _get_weight_ref_html(static_root, qualifier.year, qualifier)
+        parts.extend(
+            [
+                "        <li>",
+                f"{html.escape(qualifier.full_name)}, {weight_link} ({qualifier.year})",
+                "        </li>",
+            ]
+        )
+
+    parts.append("</ol>")
+    return parts
+
+
+def _get_placers_html_parts(
+    static_root: pathlib.Path, qualifiers: list[Qualifier]
+) -> list[str]:
+    placers: list[Qualifier] = []
+    for qualifier in qualifiers:
+        if qualifier.place is not None:
+            placers.append(qualifier)
+
+    if len(placers) == 0:
+        return []
+
+    parts: list[str] = [
+        f"<h2>State Placers ({len(placers)})</h2>",
+        "<ol>",
+    ]
+
+    for qualifier in placers:
+        weight_link = _get_weight_ref_html(static_root, qualifier.year, qualifier)
+        place_suffix = _get_placement_suffix(qualifier.place, year=qualifier.year)
+        parts.extend(
+            [
+                "        <li>",
+                f"{html.escape(qualifier.full_name)}, {weight_link}{place_suffix}",
+                "        </li>",
+            ]
+        )
+
+    parts.append("</ol>")
+    return parts
+
+
+def _get_qualifiers_html_parts(
+    static_root: pathlib.Path, qualifiers: list[Qualifier]
+) -> list[str]:
+    parts: list[str] = [
         f"      <h2>State Qualifiers ({len(qualifiers)})</h2>",
     ]
 
@@ -189,6 +255,28 @@ def _get_team_html(
             )
 
         parts.append("      </ol>")
+
+    return parts
+
+
+def _get_team_html(
+    static_root: pathlib.Path, team: TeamInfo, qualifiers: list[Qualifier]
+) -> str:
+    name = team.name_normalized
+    parts: list[str] = [
+        "<html>",
+        "  <head>",
+        f"    <title>Team: {html.escape(name)}</title>",
+        '    <link href="/css/tournament-view.fb678ab0.min.css" rel="stylesheet" />',
+        "  </head>",
+        "  <body>",
+        '    <div class="tournament-view">',
+        f"      <h1>{html.escape(name)}</h1>",
+    ]
+
+    parts.extend(_get_champs_html_parts(static_root, qualifiers))
+    parts.extend(_get_placers_html_parts(static_root, qualifiers))
+    parts.extend(_get_qualifiers_html_parts(static_root, qualifiers))
 
     parts.extend(
         [
