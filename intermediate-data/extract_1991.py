70ee82987eb8e7e96b5484734c1e076197e1fa38
Danny Hermes <daniel.j.hermes@gmail.com>
2025-07-05 19:01:55 -0500

    Get competitor list transcribed for 1991.

--- intermediate-data/extract_1991.py
+++ intermediate-data/extract_1991.py
@@ -1,5 +1,748 @@
 # Copyright (c) 2025 - Present. IKWF History. All rights reserved.
 
+"""
+The competitor list (from `1991-program.pdf`) is contained below. We'll use
+this to fill out a competitor list for this year.
+
+------------------------------------------------
+
+ADDAMS JR. HIGH SCHOOL W.C.
+119 Daniel Weber
+
+ADDISON / INDIAN TRAIL ANIMALS
+72 Mike Harrison
+112 Nick Schumacher
+173 Ken Galton
+
+ANTIOCH UPPER GRADE
+72 Lucas Dyer
+112 Stephen Zalapi
+127 Jason Breen
+135 Josh Johnston
+163 Dan Newcomb
+
+ARGENTA - OREANA W.C.
+173 Clinton Smith
+275 Randy Matas
+
+ARLINGTON CARDINALS
+68 David Stoltz
+82 Griff Powell
+87 Poncho Villarreal
+93 Sean Klatt
+99 Jim Ancona
+105 Joseph Geraci
+112 Sean Lovelace
+
+AURORA J HAWKS
+77 Wesley Young
+87 Jason Rahn
+185 Marco Ordaz
+275 Richard Reynolds
+
+BATAVIA
+119 Carlo Valentino
+153 Gregory Radish
+163 Nathanial Fookes
+
+BELVIDERE YMCA BANDITS
+87 Shawn Bouback
+93 Rob Anderson
+99 Rafael Avila
+119 Timothy Gehant
+127 Aaron Latino
+
+BETHALTO JR. HIGH SCHOOL
+60 Steven Bryant
+68 Mike Smith
+72 Jake Sullivan
+77 Brad Becker
+82 Travis Zimmerman
+82 Bill Bryant
+93 Luke Rives
+93 Jason Christeson
+99 Mike Pryor
+112 Alex Smiley
+119 Jeremy Christeson
+135 Adam Witsken
+163 Ryan Collins
+173 Ryan Peterson
+275 Jeff Sticht
+275 Terry Thornton
+
+BISMARK - HENNING
+112 Matt Terrell
+127 Kevin Temples
+135 Timothy Brant
+153 Ryan Harter
+153 Bill Baird
+163 Greg Baird
+173 Jason Crippin
+
+BLACKHAWK
+93 Henry Len Franklin
+112 Billy Hughes
+119 Germaine Lishman
+144 Willie Trezvant
+
+BLUE CREW
+77 Daniel Borland
+185 Ken Keltner
+
+BRADLEY - BOURBONNAIS
+60 Shawn Golden
+68 Russell Arseneau
+77 Brian Ebersole
+275 Michael Rossow
+
+CALUMET MEMORIAL PD
+105 Ricky Perez
+112 Michael Lucido
+119 Issac Hampton
+
+CHATHAM
+87 Richard Root
+
+CHILLICOTHE
+77 Matt Nelson
+127 Rick Babb
+144 Matt Peterson
+275 Ricardo Rodriguez
+
+CRESTWOOD COLTS
+119 Dylan Shaughnessy
+127 Ryan Stonitsch
+135 Randy Anderson
+144 Ray Todd Guzak
+163 Jason Nowicki
+275 Jason Nape
+
+CROSSFACE
+60 Steve Rodgers
+64 Ricky McDonald
+
+CRYSTAL LAKE WIZARDS
+60 Brian Creighton
+68 Bradlee Vermillion
+68 Brian Wilson
+77 Ryan Mason
+82 Alan Wilson
+93 Matthew McDonnell
+99 Jonathan Wilson
+112 David Creighton
+185 Jeffrey Alheit
+
+CUMBERLAND COUNTY
+72 Chris Thompson
+119 Clay Roan
+135 Korry Zike
+
+DANVILLE BOYS CLUB
+64 David Gnaden
+68 Moses Reed
+99 Steve Marvin
+
+DECATUR
+119 Timothy Forgey
+144 Jerome Holt
+
+DEWITT COUNTY
+144 David Delmonico
+144 Michael White
+
+DOLTON PARK FALCONS
+60 Anthony Opiola
+64 Todd Combes
+72 Pat Wrona
+72 Tony Gill
+77 Ryan Delira
+87 Josue Villasenor
+99 Joey Opiola
+119 Steven Carrol
+127 Ken Mills
+153 Pat Mai
+275 Timothy Edwards
+
+DUKES WRESTLING YORKS
+135 Jake Bricker
+173 Jack Paiti
+275 Dave Rodriquez
+
+DUNDEE HIGHLANDERS
+60 Dominick Rodriguez
+72 Michael Bondi
+
+EDISON MIDDLE
+173 Steven Havard
+
+EDWARDSVILLE
+72 Doc Sanders
+87 John Heathcott
+93 Ronnie Hansen
+153 Brent Rogers
+163 Tony Leopold
+185 Josh Champlin
+275 Brad Evans
+
+EISENHOWER JR. HIGH
+153 Jason Salley
+
+EL PASO
+72 Shawn Dewispelaere
+119 Peter Cleary
+153 Adam Mool
+173 Chris Kearney
+275 Bill Parrish
+
+ELGIN GRAPPLERS
+105 Marcus Smith
+
+ELGIN MATT RATS
+64 Robert Cardoso
+144 Ryan Seloover
+163 Matt Heiser
+
+FALCONS MAYWOOD
+60 Peter Ruffino
+
+FORMAN
+93 Zac Charlton
+99 Jeremy Passmore
+
+FRANKLIN PARK CHIEFS
+163 Daryl Phillips
+275 John Rutledge
+
+FRANKLIN PARK RAIDERS
+60 Eugene Sacco
+60 Tony Doughty
+72 Matthew Henry
+77 Craig Lorek
+82 Timothy Kazmierczak
+99 Jason Pounders
+135 Brandon Presern
+
+FROST / BAD BOYZ
+127 David Diaz
+135 Mike Lopez
+144 John Ochana
+275 Luis Aguirre
+
+GENESEO
+87 Josh Inch
+112 Matt Wilson
+135 Chad Snook
+144 Shane Hudnall
+153 Donald Hock
+163 John Rupprecht
+
+GENEVA PD
+72 Brian Comstock
+
+GEORGETOWN
+93 Michael Brucker
+127 Cecil Severado
+
+GIBSON CITY YOUTH
+60 Jared Schroeder
+93 Brian Nettleton
+105 Cord Schroeder
+163 Rob Neal
+
+GOLDEN EAGLES LTD
+72 JC Gaunt
+
+GORDON TECH RAMS
+127 Ramon Navarro
+173 Peter Mikhail
+185 Marc Speziale
+275 Eric Dongus
+
+GRANITE CITY COOLIDGE
+127 Jamie Kirby
+144 Tony Buchek
+
+GRANITE CITY GRISBY
+77 Chris Hogan
+105 TJ Slay
+
+HARLEM SCHOOL DIST. 122
+68 Bradley Schrader
+82 Robert Pyfer
+87 Ty Powers
+93 Roberto Rodriguez
+99 Aaron Ehle
+105 Ben Mena
+119 Johnny Crenshaw
+144 Peter Hackman
+
+HARVARD
+153 Chuck Blazer
+163 Ches Dougherty
+173 Tim Brown
+185 Jose Saucedo
+
+HARVEY PD TWISTERS
+64 Leonard Bowens
+68 Darnell D. Lollis
+72 Calvin Glasby
+72 Roamelle Kizzee
+72 David Douglas
+77 Milton Blakely
+82 Tony Davis
+99 Lashun Jackson
+112 Timothy Williams
+163 Omari McClellan
+173 Ricky Hudson
+275 Ricky Biggs
+
+HICKORY HILLS PD
+77 Steven Nielsen
+153 John Deligiannis
+163 Tony Zitzka
+173 Brian Billett
+
+HINSDALE RED DEVILS
+87 Derek Starr
+
+HOOPESTON-EAST LYNN JR.
+72 Lucas Aht
+99 Kevin Fredericks
+127 Ben Sollars
+153 Ben Pyle
+185 Chris McCay
+
+ILLINI BLUFFS
+68 Matt Johnson
+163 Brian Benson
+
+INDIAN PRAIRIE PIONEERS
+99 Steve Balderas
+135 Chris Kucharski
+
+IRISH CADETS
+173 Kevin Corrigan
+185 Bobby Zink
+275 Eddy Solis
+
+JOLIET BOYS CLUB COBRAS
+93 Terrance Snapp
+
+JORDON
+119 Sam Crowder
+135 Deric Schradeya
+
+KNIGHT
+82 Josh Moore
+153 Jason Sauber
+
+LAKE VILLA LANCERS
+119 James Patterson
+185 Jacob Nathan
+275 Bill Ries
+
+LEMONT
+173 Kris Hermansen
+
+LIL REAPER
+72 Jessie Flores
+87 Matt Bagley
+275 Mike Carreno
+
+LIONS
+99 Ben King
+135 Bill Zeman
+185 Andrew Dall
+
+LITTLE CELTICS
+119 Charles Crescenzo
+
+LITTLE DEVILS BELLEVILLE
+64 Nicholas Bingheim
+64 Eric Ferguson
+68 Chuck Burrows
+77 Nick Burrows
+99 Gregory Voegtle
+105 Mike Greenfield
+112 Matt Ferguson
+144 Les Whittaker
+173 Craig Eddinger
+
+LITTLE GIANTS
+68 Matt Goldstein
+185 Dan Volipendentest
+
+LOCKPORT GRAPLERS
+64 John Murphy
+82 Francisco Bermudez
+93 Robert Dela Fuente
+127 Ray Gunier
+
+MACOMB YMCA KIDS W.C.
+68 Brad Prunty
+72 Brian Prunty
+144 Andy Ball
+173 Jonnny Knight
+
+MAINE TWP. EAGLES
+77 Mike Komo
+
+MARION
+105 Andy Rains
+
+MASON SCHOOL PLAYGROUND
+105 Antwaun Scott
+
+MATTOON
+72 Cal Ryder
+87 Nick Upton
+
+MEAD JR. HIGH SCHOOL
+99 Daryl Kirk
+105 Daniel McDonough
+153 Nate Vaughn
+163 John Kern
+
+METAMORA KIDS
+60 Jason Atherton
+105 Matt Webster
+112 Rusty Smith
+119 Randy Richardson
+275 Jake Johnston
+
+MOLINE TIGERS
+77 Matthew Lackey
+82 Cody Vanklaveren
+93 Norman Jacks
+
+MORTON YOUTH
+72 Grant Hoerr
+82 Tim Glover
+87 Mike Hasty
+93 Blake Hoerr
+
+MT. ZION
+60 Shane Mendenhall
+64 Brad Owens
+68 Ronnie Dalluge
+77 Ryan Owens
+82 Drew Blickensderfer
+87 Jeff Wilson
+93 Jon Swanson
+173 Greg Homrighous
+185 Mark Harvey
+275 Matthew Freeman
+
+MURPHY JR. HIGH
+112 Ronnie Gill
+119 Richard Ripe
+127 John Hope
+135 Brian Decker
+153 Justin Bilderback
+163 Matt Major
+173 Matthew Jacobs
+173 Mark King
+185 Josh Mileur
+185 Eric Balch
+
+MUSTANGS
+82 Fran Blake
+93 Ron Winchel
+99 Chris Peterson
+105 Matt Dorgan
+112 Tom Ciezki
+144 Terry McCabe
+144 Kevin Omara
+163 Anthony Munizzi
+
+NAPERVILLE PATRIOTS
+60 Damien Billow
+72 Joel Howard
+275 Robert Kronn
+
+NAPERVILLE WARHAWKS
+163 Dan Rudny
+
+NAPERVILLE WARRIORS
+153 Jared McDonough
+153 Chet Stroschine
+
+NEWMAN BLUE DEVILS
+185 Matt Hoffmiller
+
+NORTH CHICAGO MIDGET WARHAWKS
+112 Isaac Hood
+173 Jason Harper
+275 Gregorio Bonilla
+
+NOTRE DAME
+93 Marcus Wegner
+144 Terry Pierce
+
+OAK FOREST WARRIORS
+77 Gregory Cora
+77 Kevin Randolph
+82 Tommy Lee
+87 John Evans
+87 Jake Griffin
+93 Philp Thorne
+105 Adam Wattles
+119 Robert Jenkins
+163 Nicholas Piper
+
+OAK LAWN PD WILDCATS
+68 Jesse Hewitt
+
+OAKWOOD
+105 Jimmy Talbert
+112 Adam Lyons
+
+ORLAND PARK PIONEERS
+60 Don Walsh
+64 Jason Rymut
+68 Scott Kenny
+135 Pat Otto
+144 Dan Roth
+
+OSWEGO COUGARS
+72 JEREMY LARSEN
+
+OSWEGO PANTHERS
+68 Jason Malmstrom
+144 Scott Slaughter
+185 Mike Flaar
+
+PALATINE CHARGERS
+144 Louis Sponsel
+185 Mark Comny
+
+PANTHER CUBS
+135 Robert Ertmer
+
+PANTHERS
+60 Joshua Badzinski
+64 Jeff Jezuit
+68 Troy Donnahue
+112 Timothy Donnahue
+119 Joseph Brauer
+275 Paul Naumowicz
+
+PEKIN BOYS CLUB
+99 Jeremy Boker
+
+PEORIA RAZORBACKS YOUTH
+163 Nick Knapp
+
+PEOTONE PD
+135 Dave Marevka
+163 Sam Pavesich
+
+PLAINFIELD INDIAN TRAIL
+60 Andy Konc
+68 Joseph Sukley
+127 Chad Albold
+135 Daman Montello
+144 James Hlavin
+153 Eddie Adam
+185 Ronald Becker
+
+RAIDERS
+60 Kevin Wrigley
+68 Nicholas Delisle
+87 Micheal Delisle
+
+REDBIRDS
+60 Tony Nelson
+82 Tony Niebur
+99 Rob Chester
+
+RICH WRESTLING
+99 Tim Barlog
+185 Lewis Lang
+
+RIVERDALE JR. HIGH
+64 Ben Stockwell
+99 Shawn Price
+105 Tyler Hurry
+112 Chad Orr
+127 Jon Ketelsen
+
+ROCK ISLAND
+87 Elijah Roberts
+135 Leeantwon Jones
+
+ROCKRIDGE JR. HIGH
+82 James Ealy
+153 David Freeman
+
+ROSEMONT COBRAS
+60 Tony Ganas
+64 Matthew Koulentes
+82 Luke Ewanio
+105 Trent Vensas
+112 Ryan Lee
+
+ROUND LAKE SPARTAN
+64 Amador Estrada
+64 Chad Karlen
+77 James Aberle
+77 Berry Chapman
+
+ROXANA VIKINGS
+82 Jason Hayden
+119 Thomas Riggins
+
+SPARTANS
+64 Roy Pierce
+
+ST. BARNABAS / CHRIST THE KING
+60 Matthew Earner
+64 Mike Gaskin
+68 Pat Gaskin
+72 Patrick Fahey
+77 Matthew Fahey
+82 Conor Ruel
+93 Patrick Sheahan
+93 Patrick Oconnor
+105 Dan McNulty
+127 Ryan Sheahan
+
+ST. CHARLES
+77 Aaron Gilson
+144 Steven Alf
+
+ST. TARCISSUS RAIDERS
+93 Ryan Duggan
+119 Larry Zoldowski
+127 Tim Duggan
+
+STREATOR BULLDOGS
+127 Billy Missel
+
+SYCAMORE COMMUNITY SCHOOL
+105 Philip McCormick
+127 Chris Segatto
+135 Ray Hopkins
+153 Brad Cobb
+
+TIGERTOWN TANGLERS
+60 Zachary Anderson
+64 Adam Wason
+68 Brian Clodfelter
+105 Aaron Christiansen
+173 Tony Wright
+185 Brian Smith
+
+TINLEY PARK BULLDOGS
+60 Jeffery Anderson
+87 Mike ONeill
+105 Michael Grencik
+112 Ed Howlett
+153 Tim Angsten
+
+TOMCATS
+60 Brenden Joyce
+93 Javier Parras
+99 Javier Quintanilla
+112 Gildardo Lopez
+127 Ruben Saldana
+
+TRAILBLAZERS
+68 Paul Garbis
+127 Matt Andriano
+135 Daniel Beatty
+163 Al Olieh
+185 Craig Turner
+
+TRI-CITY BRAVES
+99 Jonn Rodgers
+127 Josh Pearson
+144 Roy Bardwell
+153 Quincy Porter
+
+UNITY YOUTH
+135 Ryan Reinhart
+163 Jamie Castle
+185 David Yokley
+
+URBANA KIDS
+112 Cody Abbott
+119 Chris Hall
+
+VILLA LOMBARD COUGARS
+60 Brandon Bonati
+64 Nicholas Cirrincione
+64 Eric Woldeit
+77 Ian Penzato
+82 Michael Husslein
+82 Brian Joseph
+87 Keith Gall
+87 Leo Delatorre
+93 Kevin Milligan
+105 Peter Currie
+119 Corey Schoo
+275 Albert Licka
+
+VITTUM CATS
+82 Billy Kucinski
+87 Jim Brasher
+112 Steven Torres
+135 Mike Bertoni
+153 William Arthur
+173 Bob Rowell
+185 Chris Rowell
+
+WARHAWKS
+64 Orrin Shindell
+64 Ben Matsuda
+68 Archie Griffin
+72 Lawan Gray
+77 Alan Cartwright
+87 Justin Henry
+87 Reginald Wright
+119 Jarvis Wardlow
+153 Jason Nicolosi
+163 Damion Flowers
+173 Ben Kieckhefer
+185 Bryan Epting
+
+WAUKEGAN PD HAWKEYE
+144 Angel Morales
+173 Stanley Hayes
+
+WEST CHICAGO PD
+105 Richard Berendson
+173 Jerry Planek
+
+WEST FRANKFORT THUNDERBIRDS
+87 Matt Tomanovich
+127 Preston Deason
+135 Russell Wininger
+144 Brett Mason
+153 Matt Dawson
+
+WESTVILLE JR. HIGH
+64 Tony Skinner
+77 Shane Cox
+82 Chad Moore
+105 Luke Tuggle
+
+WHEATON WARRENVILLE
+68 Samuel Schaefer
+82 Dale Simpson
+99 Nick Macellaio
+112 Randy Divelbiss
+
+YMCA JR. FAYETTE
+99 Todd Hale
+
+YORKVILLE
+64 Steve Wernsman
+105 Clint Hornbuckle
+127 Brett Runkle
+135 Brian Runkle
+"""
+
 import pathlib
 
 import bracket_utils
