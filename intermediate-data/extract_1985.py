4ac922a0344ed6b89882bad99592056ce4ab5238
Danny Hermes <daniel.j.hermes@gmail.com>
2025-07-10 21:42:36 -0500

    Get competitor list transcribed for 1985.

--- intermediate-data/extract_1985.py
+++ intermediate-data/extract_1985.py
@@ -1,5 +1,454 @@
 # Copyright (c) 2025 - Present. IKWF History. All rights reserved.
 
+"""
+The competitor list (from scans from Corey Atwell) is contained below. We'll
+use this to fill out a competitor list for this year.
+
+------------------------------------------------
+
+55
+Joey O'Sullivan :: Colts WC :: 5
+Justin Weber :: Villa Lombard
+BYE
+Len Jankowski :: Vittum Cats :: 4
+Steve Dickinson :: Roxana PD
+Chris Cape :: Blue Crew
+Brady Curran :: Round Lake Area PD
+Tom Grennan :: Newman Middle School
+BYE
+Ryan Meagher :: New Lenox Lions :: 2
+Chad Red :: Danville Youth WC
+Mike Dickinson :: Roxana PD
+Ryan Ferguson :: Lancers :: 1
+Jerry Delira :: Lan-Oak PD Wrestling :: 6
+Mike Kearby :: Round Lake Area PD
+Andy Jenkins :: Granite City-Coolidge
+Matt Hayes :: Arlington Heights WC
+Brian LaMonica :: Orland Park Pioneers
+Boyd Stropes :: Rockridge Jr. High
+Rafeal Mejia :: Belvidere YMCA Bandits
+Rocky DeAngelo :: Dolton Falcons
+Cory Daker :: Foreman WC :: 3
+Ernie Lopez :: Lockport Grapplers
+Dan Neybert :: Panther WC
+
+60
+Daniel Schickel :: Colts WC
+Chris Hankins :: Granite City-Grigsby
+John Stanley :: Mattoon WC
+Nick Cina :: Belvidere YMCA Bandits :: 5
+Randy Berke :: DeKalb-Huntley :: 3
+Jamile Swift :: Bronco WC
+Jim Soldan :: Frankfort Falcons :: 1
+Kurt Hudson :: Redbird WC
+Jason Johnson :: Roxana PD
+Jim Virnich :: Lancers
+Damon Federighi :: Vittum Cats
+Tad Tyler :: East Moline WC
+Eric Carter :: Roxana PD :: 2
+Patick Hilton :: Lan-Oak PD Wrestling
+Dave Kinsey :: Joliet YMCA Wrestling :: 4
+Mike Mena :: Newman Middle School
+Matt Bouback :: Belvidere YMCA Bandits
+Mike Renella :: Patriots
+Chad Shaw :: Boys Club of Pekin
+Michael Griffen :: Rich Wrestling Ltd.
+Tim Stringer :: Dolton Falcons
+Tom Gahagan :: Vittum Cats :: 6
+Chad Downie :: Wheaton Falcons
+Chris Tointon :: Antioch Upper Grade
+
+65
+Dan Gilbert :: Tinley Park Bulldogs :: 3
+Carey Shearer :: Clinton Rosette M.S.
+Ryan Radosh :: Barrington Broncos
+Luke Pascale :: Orland Park Pioneers
+Peter Stanley :: Mattoon WC
+Kurt Kalchbrenner :: Vittum Cats :: 5
+Rich Weeden :: Villa Lombard :: 1
+Keith Snyder :: Panther WC
+Steve King :: Geneseo WC
+Randy Tedesco :: Belleville Little Devils
+Jerry Gille :: Belvidere YMCA Bandits
+JJ Galvin :: Foreman WC
+Louie Montez :: Geneseo WC
+Brandon Donofrio :: Gower
+Josh Mattio :: Rich Wrestling Ltd.
+Gene Bonnette :: Pekin :: 4
+Jason LeMonier :: Lan-Oak PD Wrestling :: 6
+Steve Kuhl :: Roxana PD
+Brent Laroche :: Panther WC :: 2
+Jay Ford :: New Lenox Lions
+Michael Douglas :: Harvey Twisters
+Mike Pratt :: Round Lake Area PD
+Ryan Smith :: Roxana PD
+Ferdie Deguzman :: Gower
+
+70
+Sylvester Dantzler :: Harvey Twisters
+Sean Meagher :: New Lenox Lions
+Stephan Gallardo :: Bethalto Boys Club
+Timothy Currie :: Redbird WC :: 5
+Kevin Greaney :: Eisenhower
+Shay Cordes :: East Moline WC
+Dan Pargulski :: Arlington Heights WC :: 4
+Darby Waggoner :: Lawrenceville WC
+Ralph Wals :: Lockport Grapplers
+Kelly Hamill :: Belvidere YMCA Bandits :: 2
+Manuel Olalde :: Newman Middle School
+Doug Sawyer :: Villa Lombard
+Ken Gerdes :: Orland Park Pioneers :: 1
+Aaron Roddis :: Panther Kids
+Mike Eierman :: Colts WC :: 3
+Chris Perry :: Lancers
+Mike Torres :: Vittum Cats
+Jeff Mirabella :: Elgin WC
+Jason Warner :: Roxana PD
+Martin Sanchez :: Belvidere YMCA Bandits
+Dan Walters :: Panther WC
+Eric Denning :: Warrior WC
+Jim Pesavento :: Oak Forest Warriors :: 6
+Troy Chesser :: Taylorville WC
+
+75
+Richard Harris :: Tinley Park Bulldogs :: 1
+Brett Sutphin :: Granite City-Grigsby
+Jeff Finley :: Orland Park Pioneers
+Cass Lundgren :: DeKalb-Huntley
+Christopher Perlitz :: Barrington Broncos
+Chis Seher :: Wheaton Falcons
+Andrew Gardner :: Lanphier-Southeast WC :: 5
+James Mattio :: Rich Wrestling Ltd.
+Tony Pavlak :: Belvidere YMCA Bandits
+Chris Buenik :: Vittum Cats :: 3
+John Virnich :: Lancers
+Christopher Moore :: Grandemasters
+Roger Wise :: Dundee Highlanders
+Steve Willams :: Harvey Twisters
+Marvin Fitzwater :: Georgetown WC
+Chis Shefts :: Rich Wrestling Ltd.
+Dave Neybert :: Panther WC :: 2
+Ray Callahan :: Clinton Rosette M.S.
+Todd Ryan :: Bensenville :: 4
+Shane Daker :: Foreman WC
+Tony Chierico :: Vittum Cats :: 6
+Jeff Killebrew :: Murphysboro Jr. High
+Daryl Grennan :: Newman Middle School
+Bill Walsh :: Colts WC
+
+80
+Shelly Resendez :: Oak Forest Warriors :: 3
+Eric Fincham :: Champaign
+Mike Kelly :: Joliet YMCA Wrestling
+Scott Garelli :: Villa Lombard :: 2
+Jim Jacobucci :: Crusaders WC
+Todd Vanlue :: Harvard WC
+Derek Baugh :: Bethalto Boys Club
+Shane Gibbs :: Cardinal WC
+Bret Prairie :: Bradley Bourbonnais WC
+Sam Walt :: Clinton Rosette M.S.
+Tim Thornhill :: Rich Wrestling Ltd.
+Tom Buenik :: Vittum Cats :: 6
+Priest Wilson :: Georgetown WC
+Ken Howell :: Wilbur Trimpe Jr. High
+Rody Rodriguez :: Aurora Tomcats
+Tom Derro :: Panther WC :: 5
+James Neff :: East Moline WC
+Doug Hayes :: Oak Forest Warriors :: 1
+Tom Wiliams :: Barrington Broncos
+Terrell Sandifer :: Harvey Twisters
+Darin Cowan :: Geneseo WC
+Dan Willis :: New Lenox Lions :: 4
+Rob Witt :: Bensenville
+Mark Smith :: Granite City-Grigsby
+
+85
+Scott Miedona :: Tinley Park Bulldogs :: 4
+Dan Mahler :: Oak Park-River Forest :: 2
+Michael O'Connor :: Mokena WC
+Joe Daubach :: Belleville Little Devils
+Tim Diro :: Batavia WC
+Hector Saldana :: Aurora Tomcats
+Tim Clark :: Clinton Rosette M.S.
+John St. Clair :: Rich Wrestling Ltd. :: 6
+Will Lepsi :: Vittum Cats
+Todd Oliver :: Mt. Zion WC
+Scott Abraham :: Lancers
+Tim Creighton :: Dundee Highlanders
+Jim Czajkowski :: Panther WC :: 3
+Marty Bryne :: Oak Forest Warriors
+Jeff Montgomery :: DeKalb-Huntley
+Benjy Shafer :: Blackhawk WC
+Joe Abegg :: Belleville Little Devils
+Malt McClister :: Boys Club of Pekin
+Kurt Buhle :: Joliet YMCA Wrestling
+Joe Verlinden :: East Moline WC
+Ed Enright :: Colts WC
+Mike Dusel :: Villa Lombard :: 1
+Chad Mueller :: Champaign :: 5
+Tim Cerny :: Roxana PD
+
+90
+Jim Hunziker :: Oak Forest Warriors :: 5
+Rob Crnic :: Villa Lombard
+Rich Skiles :: Rockridge Jr. High
+Dante Federighi :: Vittum Cats :: 3
+Jason Slayden :: Roxana PD
+Brett Day :: Morton Youth WC
+Shane Lyon :: Dundee Highlanders
+Scott Harris :: Geneseo WC
+Pat Coughlin :: Jefferson
+Sean Bormet :: Frankfort Falcons :: 1
+Mike Tumilty :: Boys Club of Pekin
+Brad Odum :: Carbondale PD
+Darren Ferguson :: Lancers
+Keith Dolan :: Oak Forest Warriors
+Jim Blanke :: Barrington Broncos
+Chad Hamilton :: Roxana PD :: 6
+Jim Regan :: Vittum Cats :: 4
+Tony Capodice :: Frankfort Falcons
+Tim O'Malley :: Warrior WC
+Mark Klausner :: Blackhawk WC
+Bill Fasan :: St. Barnabas
+Dion Simmons :: Generals WC
+Aaron Gaeta :: Palos South
+Stan Valle :: Crusaders WC :: 2
+
+111
+Chuck Hill :: Oak Forest Warriors
+Steve Wilcox :: Murphysboro Jr. High
+Pete Porzio :: Orland Park Pioneers
+Corey Atwell :: Geneseo WC
+Don Wicker :: Harvard WC :: 5
+Kevin Kujian :: Patriots
+Robby Grayson :: Foreman WC :: 4
+Nick Nieponski :: New Lenox Lions
+Paul Novack :: Batavia WC
+John Granat :: Vittum Cats :: 1
+Kevin Laskowski :: Jefferson
+James Drew :: Wilbur Trimpe Jr. High
+Tim Murphy :: Belvidere YMCA Bandits :: 3
+Dominick Diorio :: Colts WC
+Jim Filipiak :: Mattoon WC :: 2
+Kevin Noffsinger :: Mokena WC
+Brian French :: Arlington Heights WC
+Lucio Sotelo :: Warrior WC
+John Gagne :: Villa Lombard :: 6
+Chad Acree :: Gibson City Youth
+Nick Pfeffer :: Stickers
+Byron Overton :: Wilbur Trimpe Jr. High
+Steve Kelly :: Newman Middle School
+Matt Wake :: Colts WC
+
+118
+Joe Gilbert :: Tinley Park Bulldogs :: 1
+Jeffery Rosas :: Generals WC :: 4
+Jason Matesevac :: Joliet YMCA Wrestling
+Louis Sarris :: Naperville Warriors
+Jeff Holt :: Oak Park-River Forest
+Steve Savage :: Belvidere YMCA Bandits
+Chris Hoffman :: Granite City-Coolidge
+Scott Meyers :: Belvidere YMCA Bandits
+Pat O'Connor :: Illini Bluffs WC
+Don Neece :: Rockridge Jr. High :: 6
+Tim Daly :: Peotone PD
+Mike Swierczynski :: Franklin Park Raiders
+Sean McKeon :: Unity Youth WC :: 3
+Greg Ernest :: Belleville Little Devils
+Dan Vujovich :: Gower
+Troy Smith :: Stickers
+Jim Collin :: DeKalb-Huntley :: 5
+James Ferguson :: Tinley Park Bulldogs
+Pat Gallagly :: Harvard WC :: 2
+Oscar Olmos :: Oak Forest Warriors
+Jerod Heald :: Warrior WC
+Luke Churnovic :: Indian Trail-Plainfield
+Jason Hunter :: Indian Prairie
+Chad Logue :: Edwardsville WC
+
+125
+Eugene Browning :: Harvey Twisters
+Marc Pechter :: Weasel WC
+Kenya Scott :: Rich Wrestling Ltd.
+Rich Fenoglio :: Granite City-Coolidge :: 4
+Chad Hartwig :: Harvard WC
+Geoff Matson :: Patriots
+Shawn Muluaney :: Rockridge Jr. High :: 6
+Dan Pelton :: New Lenox Lions :: 2
+Mike McMorrow :: St. Tarcissus Raiders
+John Larson :: Illini Bluffs WC
+Dave Schenzuber :: Villa Lombard
+Mike Anderson :: Belvidere YMCA Bandits
+Dave Demumdrum :: Panther WC
+Jay Clemente :: Hickory Hills PD
+Shawn Willett :: Warrior WC :: 5
+Tim Major :: Batavia WC :: 3
+Craig Hindelang :: Edwardsville WC
+Lane Murray :: Danville Youth WC
+Paul Andreotti :: Orland Park Pioneers :: 1
+Paul Hadley :: Clinton Rosette M.S.
+Chris McMahon :: Tinley Park Bulldogs
+Matt Johnson :: Villa Lombard
+Brad Reardon :: Youth Razorbacks
+Robert Thomas :: Granite City-Grigsby
+
+135
+David Myers :: Hickory Hills PD
+Dan Driscoll :: Indian Prairie
+Tyler Donnelly :: DeKalb-Huntley
+Craig Manz :: Panther WC :: 4
+Wally Locke :: Roxana PD
+Jerry West :: Panther Kids
+Steve Hartmann :: Harvard WC :: 2
+Scott Ewing :: Rockridge Jr. High :: 5
+George Rebhan :: Naperville Warhawks
+Richie Turk :: Rich Wrestling Ltd.
+Tim Hagaman :: Illini Bluffs WC
+Kevin Lamble :: Roxana PD
+Jim Nowak :: Gower
+John Flores :: Oak Forest Warriors
+Cory Laws :: Belvidere YMCA Bandits
+Chuck Sparks :: Granite City-Coolidge :: 1
+Kyle Kennedy :: Rosemont Cobra :: 6
+Frank Muller :: Palos South
+Mark Roach :: Clinton Rosette M.S.
+Gory Anderson :: Blackhawk WC
+Peter Ruiz :: St. Barnabas
+Scott Davis :: Gibson City Youth
+David DiCarlo :: Rich Wrestling Ltd. :: 3
+Grant McClinton :: Bronco WC
+
+145
+Kevin Nolan :: Colts WC :: 1
+Mark Miller :: Wilbur Trimpe Jr. High
+Kyle Field :: Youth Razorbacks :: 6
+Brett Olsen :: Batavia WC
+Doug Anderson :: DeKalb-Huntley
+Craig Bardin :: Panther WC
+Mike Scroggins :: Joliet YMCA Wrestling
+Larry Richardson :: Unity Youth WC
+Mike Mansfield :: Granite City-Coolidge
+Matt Rood :: Villa Lombard :: 3
+Glen Gonzales :: Rosemont Cobra
+Lee Hitchock :: Rockridge Jr. High
+Marcus Adkins :: Edwardsville WC :: 5
+Russell Davis :: Chicago Ridge PD :: 4
+Mike Adsit :: Indian Trail-Plainfield
+Brett Hooghkirk :: Clinton Rosette M.S.
+Tony Iacubino :: Deerpath WC
+Scott Johnson :: Wheaton Falcons
+Michael Tison :: Catlin WC
+Dennis Giroux :: Mokena WC
+John Venderwall :: Midlothian PD Braves
+Dean McWilliams :: Vittum Cats :: 2
+Marc Fiore :: Marquart
+Jason Kleckner :: Harvard WC
+
+155
+John Conley :: Hickory Hills PD
+Joe Schwab :: Galesburg
+Rob Way :: Cardinal WC
+Jehad Hamdan :: Lemont WC :: 3
+Grant Horsch :: Fischer WC
+Wayne Stahmer :: Matburns WC
+John Fiduccia :: Bensenville :: 2
+Kevin Collins :: Vittum Cats
+Richard Hendrickson :: Rock Island 1
+Donald Fort :: West Frankfort WC
+Matt Young :: Harvard WC
+James Neland :: Morton Youth WC :: 5
+Mike Mondus :: East Moline WC
+Kevin Kulak :: Naperville Warhawks
+Tim Gettler :: Rich Wrestling Ltd.
+Darin Chambliss :: Catlin WC :: 6
+Mike Winter :: Dolton Falcons
+Mark Roswell :: Wilbur Trimpe Jr. High
+Mike Manganiello :: Vittum Cats :: 1
+Sean Kavanaugh :: Palos South
+Dan O'Hara :: Colts WC
+Phil Stuart :: Blackhawk WC
+Derek Grable :: Wilbur Trimpe Jr. High :: 4
+Bill Voigel :: West Chicago
+
+170
+Chris Gadziak :: Hickory Hills PD :: 6
+Brent Graham :: Peotone PD
+Chuck Attig :: Murphysboro Jr. High
+Dan MaGee :: Hoopeston East Lynn
+Jim Gordon :: Hinsdale
+Matt Newberry :: Jordan WC
+Sherif Zegar :: Panther WC :: 1
+Craig Wall :: Wilbur Trimpe Jr. High :: 4
+John Adams :: Palos South
+Warren Mikols :: Batavia WC
+Frank Wallock :: DeKalb-Huntley
+Doug Myhatt :: Naperville Warriors
+Erik Nelson :: Palos South
+Rick Castle :: Bismark-Henning WC
+Wilbur Watts :: Hazel Crest Jr. Hawks
+Gilbert Rivera :: Eisenhower :: 5
+Bradley George :: St. Tarcissus Raiders :: 3
+Jim Nolen :: Harvard WC
+Bill Johnson :: Edwardsville WC :: 2
+James Sheppard :: Cardinal WC
+Jim Harper :: Oak Park-River Forest
+Mike Kenison :: Warrior WC
+Chris Widlacki :: Colts WC
+Dick Frost :: Unity Youth WC
+
+185
+Randy Scianna :: Oak Forest Warriors :: 1
+Todd Wilson :: Wilbur Trimpe Jr. High
+BYE
+Tom Craig :: Galesburg
+Dan Ludford :: Antioch Upper Grade
+Paul Martinet :: Gower
+Charles Hisel :: Bradley Bourbonnais WC :: 5
+Glen Ford :: Indian Trail-Plainfield
+Chad McCrimmon :: Blackhawk WC
+Eric Keizer :: Matburns WC :: 3
+Jason Miller :: Bensenville
+Joseph Harris :: Wilbur Trimpe Jr. High
+Brian Myers :: Elgin WC :: 2
+Brian Hufnagl :: Dolton Falcons
+Scott DeWeesi :: Georgetown WC
+Dave Day :: Indian Trail-Plainfield :: 4
+Mario Gutierrez :: Oak Park-River Forest
+Marcus Wallace :: Sycamore
+John Vargus :: Naperville Warriors
+Tony Berardi :: Boys Club of Pekin
+Bill Schmitz :: Matburns WC
+Bill Dietz :: Murphysboro Jr. High :: 6
+Larry Mital :: East Moline WC
+David Seerup :: Tinley Park Bulldogs
+
+275
+Paul Lovrich :: Hickory Hills PD
+Chuck Dillman :: Champaign
+Russell Greenberg :: Rich Wrestling Ltd.
+Andy Grimm :: Villa Lombard :: 1
+Tom O'Hare :: St. Tarcissus Raiders
+Brian Rose :: Elgin WC
+Jason Hill-Selph :: Carbondale PD :: 3
+Jeff Carlson :: Machesney Park Independent :: 6
+Terry Hunter :: Pekin
+Greg Pohl :: Rockridge Jr. High
+Mark Wulf :: Mokena WC
+Philip Sisto :: Matburns WC
+Edward Schaller :: Urbana Kids Club
+Jason Burdette :: Herrin
+Tim Honn :: Jefferson :: 4
+Bill Marek :: Vittum Cats
+Ron Patrick :: Rock Island 1
+Jerry Cook :: Robbins Eagles
+Todd Martins :: Belvidere YMCA Bandits :: 2
+Mike Roe :: Dolton Falcons
+Todd Poci :: Newman Middle School
+Rich Allison :: Rich Wrestling Ltd. :: 5
+Mark Crawford :: Bensenville
+Bob Rains :: Granite City-Coolidge
+"""
+
 import pathlib
 
 import bracket_utils
