ab9c64de1c4266a51c8839fc1024b59b22f1eb0d
Danny Hermes <daniel.j.hermes@gmail.com>
2025-07-14 23:43:01 -0500

    Get competitor list transcribed for 1974.

--- intermediate-data/extract_1974.py
+++ intermediate-data/extract_1974.py
@@ -1,5 +1,300 @@
 # Copyright (c) 2025 - Present. IKWF History. All rights reserved.
 
+"""
+The competitor list (from scans from Terry Weber) is contained below. We'll
+use this to fill out a competitor list for this year.
+
+------------------------------------------------
+
+60
+COLANDRO :: Murphysboro
+SLUSARSKI :: New Lenox :: 4
+HORCHER :: Highlanders
+COBB :: Clinton
+PIERRE :: Wheaton
+GILLEY :: Belvidere
+MAHLER :: St. Doms :: 2
+ODDO :: Oak Forest :: 5
+WHALEN :: Granite City
+RUETTIGER :: Joliet Boys Club :: 3
+DEAN :: West Chicago
+OVERTON :: Mattoon
+POWERS :: Indian Trails :: 6
+BOS :: Sterling
+PRESSNEY :: Gemini Jr. High
+ED DE BEVEC :: Tinley Park :: 1
+
+65
+HARRIS :: Belleville
+GIBSON :: Antioch :: 6
+PATTON :: Woodstock
+SLUSARSKI :: New Lenox :: 2
+LUCANSKY :: Des Plaines :: 4
+GIVEN :: Lincoln, Ill.
+QUINLAN :: Tinley Park
+BROWN :: Franklin
+OLLAR :: Carterville
+BOGI :: Adams :: 5
+EWOLDT :: Wheaton
+BOB WHITLEY :: Joliet Boys Club :: 1
+GRAFF :: Gemini Jr. High
+MONTEZ :: Hoopeston
+KELLEY :: Chicago Ridge :: 3
+ANDREI :: Hoffman
+
+70
+WHALEN :: Granite City :: 2
+LANDRY :: Lincoln (Naperville)
+KILLHOFE :: Raiders
+COOKE :: Keller Jr. High :: 5
+GERDES :: Oak Forest  :: 3
+VERCELOTTI :: Troy
+CHAMPION :: East Moline
+GRANT :: Hoopeston
+KOONS :: Murphysboro
+WALTERS :: Rosette
+HURWITZ :: London Jr. High
+FOWLER :: West Chicago :: 4
+KELLY :: Chicago Ridge
+DANE NASENBENNY :: Joliet Boys Club :: 1
+SCHINVILLE :: Sterling
+SAMPSON :: Mahomet—Seymour :: 6
+
+75
+COLYOTTE :: Granite City
+EVANS :: Elk Grove :: 6
+BRYANT :: Tinley Park :: 2
+STEWART :: Rosette
+DOMBROWSKI :: Sterling :: 4
+GLUCK :: Ke;ler
+MURRAY :: Mahomet—Seymour
+SUMIDA :: Downers Grove
+SCHNAUTZ :: Murphysboro
+UJIIYE :: Gemini Jr. High
+STEVENS :: Tinley Park :: 3
+GARARD :: Lincoln (Naperville)
+OLALDE :: Sterling :: 5
+HELMINSKI :: Highlanders
+CHECK :: Lincoln, Ill.
+MARK TREZZINO :: Joliet Boys Club :: 1
+
+80
+MILITELLO :: East Alton
+McMAHON :: Tinley Park :: 2
+SCHINVILLE :: Sterling
+SUTCLIFFE :: Apollo Jr. High :: 4
+MARIANI :: Bloomington :: 6
+McCUE :: Simmons (Aurora)
+ROTH :: Joliet Boys Club
+BLOUNT :: Keller Jr. High
+BASORE :: Granite City
+GERDES :: Oak Forest
+CAMPOS :: Rock Falls
+POLZ :: Raiders :: 3
+PHILLIPS :: Catlin
+EWOLDT :: Wheaton
+STERR :: Joliet Boys Club :: 5
+DAN UNRUH :: West Chicago :: 1
+
+85
+EADS :: Jerseyville
+REGLIN :: Sterling :: 2
+IBEN :: Champaign Jr. High
+BEECHY :: Oak Forest :: 6
+CARTER :: Woodridge :: 4
+CONRAD :: Raiders
+RICE :: Adams
+McCUE :: Simmons (Aurora)
+DAHM :: Belleville
+KINKEAD :: East Moline :: 5
+SLIFER :: Charleston
+DAIDONE :: Oak Forest
+McTAGUE :: Morris
+BOB PORTER :: Northfield Pk. Dist. :: 1
+HORN :: Highlanders
+DAVIS :: Huntley :: 3
+
+90
+VOORHEES :: East Alton
+HOBBS :: Catlin
+WELLNER :: Joliet Boys Club :: 3
+TUCKER :: Franklin
+TONY CRUZ :: West Chicago :: 1
+QUINLAN :: Tinley Park
+GENSLER :: Lincoln (Naperville)
+BLACKMAN :: Raiders :: 6
+JANICEK :: Belleville
+GOODWIN :: Urbana Jr. High
+STERIOTI :: Woodridge
+COPTENS :: Coolidge :: 4
+PERRY :: Bensonville :: 5
+KELLY :: Chicago Ridge :: 2
+CHERRY :: Huntley
+KERKES :: Gemini Jr. High
+
+97
+VOORHEES :: East Alton
+SLUSARSKLI :: New Lenox
+MARGIOTTA :: Highlanders :: 4
+ALLMAN :: Chanute A.F.B.
+BURGOS :: Rosette :: 6
+BALLARD :: Sterling
+PHIL WALTERS :: Des Plaines :: 1
+CAPODICE :: Tinley Park
+HOESE :: Granite City
+QUAS :: Plainfield :: 5
+FOWLER :: West Chicago
+CHAPMAN :: Fisher
+VARGA :: Indian Trails
+VERBEKE :: Wilson
+KRIST :: Raiders :: 3
+FERREIRA :: Oak Forest :: 2
+
+105
+DUCKWORTH :: Granite City
+McGRATH :: Mundelein
+SUMMERS :: Huntley :: 5
+DANIELS :: Troy
+MILLER :: Holmes :: 3
+TWITTEY :: Mattoon
+BOTHWELL :: Tinley Park :: 2
+CORTEZ :: East Moline
+RUSSO :: Edwardsville
+LUNDQUIST :: West Chicago :: 6
+KNIGHT :: Wheaton
+CHANCEY :: Plainfield
+MARTINEZ :: Holmes
+IBEN :: Champaign Jr. High
+GASCA :: Conrady :: 4
+BOB RYAN :: Sterling :: 1
+
+112
+TERRY :: Murphysboro
+KELLY :: Lincoln (Naperville)
+RICH YALE :: Northfield Pk. Dist. :: 1
+PERALES :: Frost :: 5
+NOAK :: Tinley Park :: 3
+COLBERT :: New Lenox
+CARLSON :: East Moline
+POWELL :: Mattoon
+LEITTNER :: Edwardsville
+KOEHLING :: Huntley :: 4
+POTTS :: Wilmette
+MANUSOS :: Lake Villa
+SANDS :: Scanlon :: 6
+DILLMAN :: Troy :: 2
+HERNANDEZ :: Sterling
+SEKANDER :: Mahomet—Seymour
+
+118
+DARRELL HASTY :: Granite City :: 1
+SCHOONOVER :: Holmes
+ZOLNER :: Tinley Park :: 5
+FAIVRE :: Huntley
+PEAL :: Geneseo
+FARLENZA :: Cooper
+SCHOLES :: Charleston
+SMITH :: Woodridge :: 4
+STEWART :: Granite City :: 6
+NASATIR :: Apollo Jr. High
+KLEINSCHMIDT :: Oak Forest
+HAMMETT :: Huntley :: 2
+LAWSON :: Rock Falls
+GOBE :: West Chicago :: 3
+PETERSON :: Charleston
+POWELL :: Troy
+
+125
+ELISON :: Granite City
+GRIFFIN :: Oak Forest
+CAVAZOS :: Sterling :: 6
+OTTE :: Raiders
+CREEK :: Raiders
+WILLIAMS :: Huntley :: 4
+THOMAS :: Hinsdale
+ZIMMERMAN :: Highlanders :: 2
+HARTMAN :: Carterville
+GESIORSKI :: Central Stickney
+NICOSON :: Franklin
+BRENNAN :: Gemini Jr. High :: 5
+REED :: Hoopeston
+CHIRICO :: Lincoln (Naperville) :: 3
+TWAIT :: Wilmington
+ED PINEDA :: West Chicago :: 1
+
+134
+BURKETT :: Centralia
+NISKA :: Wilson :: 6
+OWENSBY :: Hoopeston
+GUTHRIE :: Park Forest
+RICK SANDERS :: Troy :: 1
+MARCOTTE :: Gemini Jr. High
+REYES :: West Chicago :: 3
+DODMEAD :: Indian Trails
+FORSHEE :: Granite City
+BEHRENS :: Coolidge
+GREG :: Charleston :: 4
+PHILLIPS :: Hazelcrest :: 2
+WILMSON :: New Lenox
+SOMMERS :: Gemini Jr. High
+MYLES :: Zion
+CLARK :: Lincoln (Naperville) :: 5
+
+143
+HARTLINE :: Granite City :: 3
+PAGE :: Hoopeston
+INGOLD :: Glen Ellyn
+PAPINI :: Marshall
+BLACK :: West Chicago :: 5
+ERICKSON :: Matteson
+TRENT TAYLOR :: Huntley :: 1
+SUAREZ :: Des Plaines
+HOWERTON :: Edwardsville :: 6
+DONNA :: Charleston
+GAUHR :: Glen Ellyn
+BLATZ :: Rock Falls
+BRENNAN :: Cooper :: 2
+RUBENOW :: Matteson
+WALT :: Rosette
+GOLDFINE :: Golf Jr. High :: 4
+
+152
+SCHANUEL :: Granite City :: 6
+SUMEVA :: Downers Grove :: 1 [[Dave Conciera :: Des Plaines Gemini??]]
+McNALLY :: Eisenhower Jr. High
+BROWN :: Chanute A.F.B.
+GRUBBS :: Indian Trail
+VANCIL :: Sterling
+GARRETT :: Raiders :: 3
+NOLAN :: South Stickney
+KAIRER :: Edwardsville
+PHELPS :: Troy :: 2
+TEPLER :: West Chicago :: 5
+THRIFT :: Urbana Jr. High
+FEWS :: Simmons
+POSTON :: Franklin :: 4
+SPIKE :: London Jr. High
+SCHMITT :: Oak Forest
+
+275
+LAMP :: Granite City
+MORENO :: Highlanders
+KURT KARAS :: Simmons :: 1
+ROCKOVSKI :: Morris :: 3
+PERTO :: St. Doms :: 5
+WHITE :: Mattoon
+POTACKI :: Hickory Hilis
+POWERS :: Geneseo
+NELSON :: Edwardsville
+BELLO :: Frost :: 4
+SCHMIDT :: Lincoln (Naperville)
+MALONEY :: Woodridge
+BANKSON :: Raiders :: 2
+TYLER :: Urbana Jr. High
+METZ :: Conrady
+JOHNSTON :: East Moline :: 6
+"""
+
 import pathlib
 
 import bracket_utils
@@ -20,7 +315,7 @@ _SENIOR_CHAMPS: dict[int, bracket_utils.Placer] = {
     118: bracket_utils.Placer(name="Darrell Hasty", team="Granite City"),
     125: bracket_utils.Placer(name="Ed Pineda", team="West Chicago"),
     134: bracket_utils.Placer(name="Rick Sanders", team="Shorewood Troy"),
-    143: bracket_utils.Placer(name="Trent Tayor", team="DeKalb Huntley"),
+    143: bracket_utils.Placer(name="Trent Taylor", team="DeKalb Huntley"),
     152: bracket_utils.Placer(name="Dave Conciera", team="Des Plaines Gemini"),
     275: bracket_utils.Placer(name="Kurt Karras", team="Aurora Simmons"),
 }
