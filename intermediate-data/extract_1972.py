226df18a6fc4ea4e2f6f41fbbb1b96d3732f0d78
Danny Hermes <daniel.j.hermes@gmail.com>
2025-07-15 21:52:51 -0500

    Get competitor list transcribed for 1972.

--- intermediate-data/extract_1972.py
+++ intermediate-data/extract_1972.py
@@ -1,5 +1,299 @@
 # Copyright (c) 2025 - Present. IKWF History. All rights reserved.
 
+"""
+The competitor list (from scans from Terry Weber) is contained below. We'll
+use this to fill out a competitor list for this year.
+
+------------------------------------------------
+
+60
+GERDES :: Arbor Park
+CARPENTER :: Holmes
+HARRIS :: Belleville
+UNRUH :: West Chicago
+PATTON :: Granite City
+OLALDE :: Sterling
+SLUSARSKI :: St. Mary's
+EVANS :: Elk Grove
+WHELAN :: Granite City
+PATTON :: Woodstock
+PAT STERR :: Joliet Boys Club :: 1
+LUCANSKI :: Des Plaines
+NASENBENNY :: Joliet Boys Club
+BLACKMAN :: Shabbona Park
+MILETELLO :: East Alton
+ROSENTHAL :: Moline
+
+65
+PORTER :: Northfield
+HANNER :: East Alton
+TOM GERDES :: Arbor Park :: 1
+DAMBRUSH :: Sterling
+IPPOLITE :: Tinley Park
+DLUGOPOLSKI :: West Chicago
+GROSS :: Gemini Jr.
+HOWELL :: Granite City
+SWANK :: Joliet Boys Club
+BARR :: Huntley
+MANIKA :: Fraziers
+REID :: Granite City
+LUCANSKY :: Des Plaines
+PATTON :: Granite City
+JACKSON :: New Lenox
+TURNER :: Rosette
+
+70
+KELLEMS :: Scanlon
+BASORE :: Granite City
+PORTER :: Northfield
+BURNS :: Rosette
+CONRAD :: Shabbona Park
+UNRUH :: West Chicago
+JEFFREY :: New Lenox
+WHELAN :: Granite City
+LEE GOLDSMITH :: Northfield :: 1
+BURGOS :: Rosette
+CRISCIONE :: New Lenox
+BOLERJACK :: Carmi
+VERCELOTE :: Troy
+HENDRICK :: Murphysboro
+SILCROFT :: Gemini Jr.
+SCHAFER :: Sterling
+
+77
+STERR :: Joliet
+FALLEN :: Fraziers
+McCULLOUGH :: Granite City
+RYAN :: Sterling
+McGLYNN :: Belleville
+COX :: West Chicago
+BOTHWELL :: Tinley Park
+JIM BARRETT :: Apollo Jr. High :: 1
+JANICIK :: Belleville
+BURKE :: Rosette
+McGHEE :: Palos South
+GADDI :: Deerpath
+SCHUMACHER :: Joliet
+RUH :: Fraziers
+BENNETT :: East Alton
+LARSON :: Huntley
+
+83
+FACTOR :: Gemini Jr.
+JANICIK :: Belleville
+KENNY :: Darkin Park
+BLACK :: West Chicago
+CABRERA :: Joliet
+BERGEN :: Rock Falls
+VERMILLION :: Highlanders
+BAKER :: Murphysboro
+HUTTENHOFF :: New Lenox
+CUNZ :: Huntley
+WALTERS :: Des Plaines
+ALLGIRE :: Belleville
+McAVOY :: Apollo Jr. High
+CONNELL :: Murphysboro
+HAYSE :: Morris
+JOE WILLIAMS :: Huntley :: 1
+
+90
+JOHN JACKSON :: New Lenox :: 1
+SZELES :: Granite City
+UJIIYE :: Gemini Jr.
+DODD :: West Chicago
+WHITTFIELD :: Glenview
+ROGERS :: Huntley
+EVANS :: Joliet
+BLESOE :: Granite City
+DRIBIN :: Apollo Jr. High
+MILLER :: Moline
+DURGO :: Morris
+GAINES :: Granite City
+MURPHY :: Hazel Crest
+HALL :: Murphysboro
+HELMINSKI :: Highlanders
+GUERRERO :: West Chicago
+
+97
+JEFF GERDES :: Arbor Park :: 1
+DALTON :: Cooper
+CLEMONS :: Edwardsville
+BUSSLEE :: West Chicago
+CARPENTER :: Centralia
+PROVOST :: West Chicago
+MONREAIL :: Tinley Park
+FEICH :: Des Plaines
+TOLLE :: Edwardsville
+GRIFFIN :: Rock Falls
+BEECHY :: Arbor Park
+IWASAKI :: Fraziers
+MUELLER :: Huth
+FISKE :: Apollo Jr. High
+HODSHIRE :: Granite City
+PUEBIA :: Moline
+
+105
+DE BOO :: Shabbona Park
+LITTLEJOHN :: Granite City
+MARCOTTE :: Tinley Park
+BURLEY :: West Chicago
+FOSTER :: Joliet
+CERVANTES :: Rock Falls
+BARRON :: London
+BURKETT :: Centralia
+CHIP CONNOR :: New Lenox :: 1
+RICHMOND :: Sterling
+THOMAS :: Schaumberg
+MILTON :: Granite City
+BEAM :: Des Plaines
+SLEMER :: Edwardsville
+McCUE :: Simmons
+PASTENS :: Huntley
+
+112
+TRIZZINO :: Joliet
+BURKE :: Murphysboro
+MORRIS :: (unattached)
+PLADA :: Rock Falls
+BARRINGER :: Des Plaines
+FULLMER :: Rock Falls
+STEEN :: New Lenox
+AVEDESIAN :: Granite City
+CARL SCHULTZ :: Northbrook :: 1
+PERMA :: Rock Falls
+MOORE :: Tinley Park
+HOFFMAN :: Edwardsville
+CHANDLER :: Joliet
+BENJAMIN :: Centralia
+KILRAIN :: Schaumberg
+BINWELL :: Rosette
+
+118
+KING MUELLER :: Westview :: 1
+WALTERS :: Apollo Jr. High
+WEBER :: Edwardsville
+KNUDSEN :: Moline
+ALLAN :: Jerseyville
+HOGGARD :: Moline
+VELAZQUES :: Simmons
+THOMAS :: Northfield
+LEE :: Belleville
+TAYLOR :: Huntley
+DEGARMO :: Simmons
+THEDFORD :: Fraziers
+PASSLEY :: Simmons
+CAVAZOS :: Cooper
+SEARHART :: Murphysboro
+WILLIAMS :: Sterling
+
+125
+TAIT :: Apollo Jr. High
+WEBSTER :: Belleville
+GONZALES :: Joliet
+McMILLAN :: Sycamore
+TYNER :: New Lenox
+SHERIBAN :: Moline
+WILD :: Des Plaines
+UNANGEP :: Edwardsville
+WILMS :: Tinley Park
+HARSETH :: McHenry
+OLBRISH :: Gemini Jr.
+BALWIN :: East Alton
+McGINNIS :: London
+BURKE :: Murphysboro
+WARF :: Kirby
+RON CUEVAS :: Sterling :: 1
+
+132
+WATSON :: Simmons
+COUNTS :: Jerseyville
+DE WAR :: Holmes
+DIR :: Sterling
+BARRISH :: Gemini Jr.
+SANDERSON :: Rosette
+ALBERICO :: Troy
+WOPAT :: Edwardsville
+JIM MOUROUKAS :: Des Plaines :: 1
+BUEHLER :: Huntley
+MAGER :: New Lenox
+FLOWERS :: Murphysboro
+PACHECO :: Westview
+WADE :: Jerseyville
+GOERGIN :: Schaumberg
+ZIDANS :: Sterling
+
+138
+GREG ENERSON :: Morris :: 1
+VAN WALLEGHAN :: Jerseyville
+HEY :: Sterling
+ALDRIGES :: Granite City
+REGLIN :: Sterling
+GRAHAM :: Troy
+CASHMORE :: Prospect Heights
+HARTMAN :: Granite City
+PRATT :: Rock Falls
+SUVINSKI :: Mokena
+WATSON :: Deerpath
+MOSS :: Troy
+IDRIZOWICZ :: London
+SWAN :: Granite City
+AUSTIN :: Huntley
+
+145
+De CHAMBRE :: London
+LEWIS :: Carmi
+ROBINSON :: Huth
+BONNELL :: Rock Falls
+POTERALA :: West Chicago
+RAMBI :: Orland Park
+NELSON :: Holmes
+GELLESPIE :: Belleville
+GEHRIG DERGO :: Morris :: 1
+MEIER :: Savanna
+BELL :: Palatine
+GREER :: Jerseyville
+CORR :: Des Plaines
+GUETERSLOH :: Murphysboro
+GALARZA :: Mokena
+BRUGOTH :: West Chicago
+
+152
+HRVATIN :: Joliet
+BYE
+EDDY :: Carol Stream
+AGBEROECK :: Moline
+FULLER :: Des Plaines
+HILLSTRAND :: Huntley
+MANNS :: New Lenox
+MILEUR :: Murphysboro
+GRABLE :: London
+GRIFFIN :: Moline
+SCHWARTZ :: Troy
+BYE
+ALABAN :: Morris
+LUND :: Belleville
+KRAUSS :: Cooper
+TIM CAIRNS :: Rock Falls :: 1
+
+275
+DAMIANI :: Bloom
+KRUEGER :: Holmes
+HARRING :: Edwardsville
+CLIFTON :: Huntley
+McCOY :: Edwardsville
+MITAL :: Moline
+FORREST :: New Lenox
+ROB BOEHMER :: Carol Stream :: 1
+PATTERSON :: Granite City
+RICH :: Huntley
+HOERR :: Simmons
+BARDEL :: Glenwood
+STRONK :: Kirby
+KLOCKE :: Deerpath
+BATEMAN :: Murphysboro
+PERRONEZ :: Rock Falls
+"""
+
 import pathlib
 
 import bracket_utils
